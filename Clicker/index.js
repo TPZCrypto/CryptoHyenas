7c1469a397de1c03fdd9a62ad1e28743bae93694
TPZCrypto <166454807+TPZCrypto@users.noreply.github.com>
2024-04-25 01:57:25 +0300

    Update index.js

--- Clicker/index.js
+++ Clicker/index.js
@@ -2,17 +2,17 @@ money = 0;
 moneyup = 1;
 msec = 0;
 upcost = 15;
-smallhyenascost = 25;
-bighyenascost = 250;
+catcost = 25;
+workercost = 250;
 upown = 0;
-smallhyenasown = 0;
-bighyenasown = 0;
-smallhyenasadd = 1;
-bighyenasadd = 15;
+catown = 0;
+workerown = 0;
+catadd = 1;
+workadd = 15;
 cboost = 1;
 wboost = 1;
-smallhyenasmax = 0;
-bighyenasmax = 0;
+catmax = 0;
+workmax = 0;
 
 //save before exiting
 function closingCode() {
@@ -30,10 +30,10 @@ function reloadall() {
   document.getElementById("click").innerHTML =
     "Hyenas/click: " + addcomma(moneyup) + " | Hyenas/sec: " + addcomma(msec);
   document.getElementById("total").innerHTML = "Hyenas: " + addcomma(money);
-  document.getElementById("smallhyenas").innerHTML =
-    smallhyenasown + "-Small Hyenas: " + addcomma(smallhyenascost) + " | +" + addcomma(smallhyenasadd) + "/sec";
-  document.getElementById("bighyenas").innerHTML =
-    bighyenasown + "-Big Hyenas: " + addcomma(bighyenascost) + " | +" + addcomma(bighyenasadd) + "/sec";
+  document.getElementById("cat").innerHTML =
+    catown + "-clicker cat: " + addcomma(catcost) + " | +" + addcomma(catadd) + "/sec";
+  document.getElementById("worker").innerHTML =
+    workerown + "-worker: " + addcomma(workercost) + " | +" + addcomma(workadd) + "/sec";
   document.getElementById("upgrade").innerHTML =
     addcomma(upown) + "-main upgrade: " + addcomma(upcost);
 }
@@ -43,19 +43,19 @@ function save() {
   localStorage.setItem("moneyup", moneyup);
   localStorage.setItem("msec", msec);
   localStorage.setItem("upcost", upcost);
-  localStorage.setItem("smallhyenascost", smallhyenascost);
-  localStorage.setItem("smallhyenasadd", smallhyenasadd);
-  localStorage.setItem("bighyenascost", bighyenascost);
-  localStorage.setItem("bighyenasadd", bighyenasadd);
-  localStorage.setItem("smallhyenasown", smallhyenasown);
-  localStorage.setItem("bighyenasown", bighyenasown);
+  localStorage.setItem("catcost", catcost);
+  localStorage.setItem("catadd", catadd);
+  localStorage.setItem("workercost", workercost);
+  localStorage.setItem("workadd", workadd);
+  localStorage.setItem("catown", catown);
+  localStorage.setItem("workerown", workerown);
   localStorage.setItem("upown", upown);
-  localStorage.setItem("smallhyenasadd", smallhyenasadd);
-  localStorage.setItem("bighyenasadd", bighyenasadd);
+  localStorage.setItem("catadd", catadd);
+  localStorage.setItem("workadd", workadd);
   localStorage.setItem("cboost", cboost);
   localStorage.setItem("wboost", wboost);
-  localStorage.setItem("smallhyenasmax", smallhyenasmax);
-  localStorage.setItem("bighyenasmax", bighyenasmax);
+  localStorage.setItem("catmax", catmax);
+  localStorage.setItem("workmax", workmax);
 }
 //loads save file
 function load() {
@@ -63,19 +63,19 @@ function load() {
   moneyup = parseInt(localStorage.getItem("moneyup"));
   msec = parseInt(localStorage.getItem("msec"));
   upcost = parseInt(localStorage.getItem("upcost"));
-  smallhyenascost = parseInt(localStorage.getItem("smallhyenascost"));
-  upown = parseInt(localStorage.getItem("smallhyenasadd"));
-  bighyenascost = parseInt(localStorage.getItem("bighyenascost"));
-  upown = parseInt(localStorage.getItem("bighyenasadd"));
-  smallhyenasown = parseInt(localStorage.getItem("smallhyenasown"));
-  bighyenasown = parseInt(localStorage.getItem("bighyenasown"));
+  catcost = parseInt(localStorage.getItem("catcost"));
+  upown = parseInt(localStorage.getItem("catadd"));
+  workercost = parseInt(localStorage.getItem("workercost"));
+  upown = parseInt(localStorage.getItem("workadd"));
+  catown = parseInt(localStorage.getItem("catown"));
+  workerown = parseInt(localStorage.getItem("workerown"));
   upown = parseInt(localStorage.getItem("upown"));
-  smallhyenasadd = parseInt(localStorage.getItem("smallhyenasadd"));
-  bighyenasadd = parseInt(localStorage.getItem("bighyenasadd"));
+  catadd = parseInt(localStorage.getItem("catadd"));
+  workadd = parseInt(localStorage.getItem("workadd"));
   cboost = parseInt(localStorage.getItem("cboost"));
   wboost = parseInt(localStorage.getItem("wboost"));
-  smallhyenasmax = parseInt(localStorage.getItem("smallhyenasmax"));
-  bighyenasmax = parseInt(localStorage.getItem("bighyenasmax"));
+  catmax = parseInt(localStorage.getItem("catmax"));
+  workmax = parseInt(localStorage.getItem("workmax"));
 
   reloadall();
 }
@@ -86,13 +86,13 @@ function reset() {
     moneyup = 1;
     msec = 0;
     upcost = 15;
-    smallhyenascost = 25;
-    bighyenascost = 250;
-    smallhyenasown = 0;
-    bighyenasown = 0;
+    catcost = 25;
+    workercost = 250;
+    catown = 0;
+    workerown = 0;
     upown = 0;
-    smallhyenasadd = 1;
-    bighyenasadd = 15;
+    catadd = 1;
+    workadd = 15;
     reloadall();
   }
 }
@@ -111,88 +111,88 @@ function clicked() {
 //upgrade function
 function upgrade(name) {
   if (name == "Small Hyenas") {
-    if (money >= smallhyenascost && smallhyenasown < 50) {
+    if (money >= catcost && catown < 50) {
       
-      if (smallhyenasown <= 13) {
-        msec += smallhyenasadd;
-        smallhyenasadd++;
+      if (catown <= 13) {
+        msec += catadd;
+        catadd++;
         cboost = 1;
-      } else if (smallhyenasown == 14) {
-        msec += smallhyenasadd;
-        smallhyenasadd++;
+      } else if (catown == 14) {
+        msec += catadd;
+        catadd++;
         cboost = 200;
-      } else if (smallhyenasown <= 23) {
-        msec += 200 * smallhyenasadd;
-        smallhyenasadd++;
+      } else if (catown <= 23) {
+        msec += 200 * catadd;
+        catadd++;
         cboost = 200;
-      } else if (smallhyenasown == 24) {
-        msec += 200 * smallhyenasadd;
-        smallhyenasadd++;
+      } else if (catown == 24) {
+        msec += 200 * catadd;
+        catadd++;
         cboost = 5000;
-      } else if (smallhyenasown <= 48) {
-        msec += 5000 * smallhyenasadd;
-        smallhyenasadd++;
+      } else if (catown <= 48) {
+        msec += 5000 * catadd;
+        catadd++;
         cboost = 5000;
-      } else if (smallhyenasown == 49) {
-        msec += 5000 * smallhyenasadd;
-        smallhyenasadd++;
+      } else if (catown == 49) {
+        msec += 5000 * catadd;
+        catadd++;
         cboost = 15000;
       } else {
-        msec += 15000 * smallhyenasadd;
-        smallhyenasadd++;
+        msec += 15000 * catadd;
+        catadd++;
         cboost = 15000;
       }
-      smallhyenasown += 1;
-      money -= smallhyenascost;
-      smallhyenascost = smallhyenascost * 2;
-      document.getElementById("smallhyenas").innerHTML =
-        smallhyenasown + "-Small Hyenas: " + addcomma(smallhyenascost) + " | +" + addcomma(smallhyenasadd * cboost) + "/sec";
-    } else if (smallhyenasown == 50) {
-      document.getElementById("smallhyenas").innerHTML =
-        smallhyenasown + "-Small Hyenas: MAX | +15% click/sec";
+      catown += 1;
+      money -= catcost;
+      catcost = catcost * 2;
+      document.getElementById("cat").innerHTML =
+        catown + "-clicker cat: " + addcomma(catcost) + " | +" + addcomma(catadd * cboost) + "/sec";
+    } else if (catown == 50) {
+      document.getElementById("cat").innerHTML =
+        catown + "-clicker cat: MAX | +15% click/sec";
     }
   }
 
   if (name == "Big Hyenas") {
-    if (money >= bighyenascost && bighyenasown < 50) {
+    if (money >= workercost && workerown < 50) {
       
-      if (bighyenasown <= 13) {
-        msec += bighyenasadd;
-        bighyenasadd++;
+      if (workerown <= 13) {
+        msec += workadd;
+        workadd++;
         wboost = 1;
-      } else if (bighyenasown == 14) {
-        msec += bighyenasadd;
-        bighyenasadd++;
+      } else if (workerown == 14) {
+        msec += workadd;
+        workadd++;
         wboost = 200;
-      } else if (bighyenasown <= 23) {
-        msec += 200 * bighyenasadd;
-        bighyenasadd++;
+      } else if (workerown <= 23) {
+        msec += 200 * workadd;
+        workadd++;
         wboost = 200;
-      } else if (bighyenasown == 24) {
-        msec += 200 * bighyenasadd;
-        bighyenasadd++;
+      } else if (workerown == 24) {
+        msec += 200 * workadd;
+        workadd++;
         wboost = 5000;
-      } else if (bighyenasown <= 48) {
-        msec += 5000 * bighyenasadd;
-        bighyenasadd++;
+      } else if (workerown <= 48) {
+        msec += 5000 * workadd;
+        workadd++;
         wboost = 5000;
-      } else if (bighyenasown == 49) {
-        msec += 5000 * bighyenasadd;
-        bighyenasadd++;
+      } else if (workerown == 49) {
+        msec += 5000 * workadd;
+        workadd++;
         wboost = 15000;
       } else {
-        msec += 15000 * bighyenasadd;
-        bighyenasadd++;
+        msec += 15000 * workadd;
+        workadd++;
         wboost = 15000;
       }
-      bighyenasown += 1;
-      money -= bighyenascost;
-      bighyenascost = bighyenascost * 3;
-      document.getElementById("bighyenas").innerHTML = 
-        bighyenasown + "-Big Hyenas: " + addcomma(bighyenascost) + " | +" + addcomma(bighyenasadd * wboost) + "/sec";
-    } else if (bighyenasown == 50) {
-      document.getElementById("bighyenas").innerHTML =
-        bighyenasown + "-Big Hyenas: MAX | +35% click/sec";
+      workerown += 1;
+      money -= workercost;
+      workercost = workercost * 3;
+      document.getElementById("worker").innerHTML = 
+        workerown + "-worker: " + addcomma(workercost) + " | +" + addcomma(workadd * wboost) + "/sec";
+    } else if (workerown == 50) {
+      document.getElementById("worker").innerHTML =
+        workerown + "-worker: MAX | +35% click/sec";
     }
   }
 
@@ -204,15 +204,15 @@ function upgrade(name) {
       upcost = upcost * 5;
       document.getElementById("upgrade").innerHTML =
         addcomma(upown) + "-main upgrade: " + addcomma(upcost);
-      if (smallhyenasown == 50) {
-        msec -= smallhyenasmax;
-        smallhyenasmax = Math.floor(moneyup * 0.15);
-        msec += smallhyenasmax;
+      if (catown == 50) {
+        msec -= catmax;
+        catmax = Math.floor(moneyup * 0.15);
+        msec += catmax;
       }
-      if (bighyenasown == 50) {
-        msec -= bighyenasmax;
-        bighyenasmax = Math.floor(moneyup * 0.35);
-        msec += bighyenasmax;
+      if (workerown == 50) {
+        msec -= workmax;
+        workmax = Math.floor(moneyup * 0.35);
+        msec += workmax;
       }
     }
   }
